da7fea17ea62b4f0c3756a0b40bc7201a570ccc1
Edwin Mahendra <edwin.mahendra@ti.ukdw.ac.id>
2023-06-06 13:53:08 +0700

    Add lot of features (auto-correct, menu_makanan, etc)

--- chatbot_backend.py
+++ chatbot_backend.py
@@ -1,49 +1,51 @@
-import aiml, os
+import aiml
+import os
+
 from flask import Flask, render_template, request, jsonify
-from flask_cors import CORS, cross_origin
+from flask_cors import CORS
+from auto_correct import correction
 
-# creating kernel and learning AIML files
-kernel = aiml.Kernel()
+class Bot:
+    def __init__(self, aiml_kernel=None):
+        self.kernel = aiml_kernel if aiml_kernel else aiml.Kernel()
+        self._learn_aiml()
 
-# Faster if bootstrap already exists
-if os.path.isfile("kbot.brn"):
-    kernel.bootstrap(brainFile="kbot.brn")
-else:
-    kernel.bootstrap(learnFiles="start.xml", commands="KFC")
-    kernel.saveBrain("kbot.brn")
+    def _learn_aiml(self):
+        if os.path.isfile("kbot.bot"):
+            self.kernel.bootstrap(brainFile="kbot.bot")
+        else:
+            self.kernel.bootstrap(learnFiles="start.xml", commands="KFC")
+            self.kernel.saveBrain("kbot.bot")
 
-def get_respond_from_backend(msg):
-    print(f"Input message: {msg}")
-    response = kernel.respond(msg)
-    print(f"Kernel response: {response}")
-    
-    if response:
-        return response
-    else:
-        return "Maaf... saya kurang mengerti"
+    def get_response(self, pesan):
+        response = self.kernel.respond(pesan)
+        return response if response else "Maaf... saya kurang mengerti"
 
-app = Flask(__name__)
-CORS(app) # This will enable CORS for all routes
+def create_app():
+    app = Flask(__name__)
+    CORS(app)
+    bot = Bot()
 
-@app.route("/")
-def home():
-    return render_template("base.html")
+    @app.route("/")
+    def home():
+        return render_template("base.html")
 
-@app.route("/predict", methods=['POST'])
-@cross_origin() # This will enable CORS for this route
-def send_message():
-    msg = request.get_json()["message"]
-    respond = get_respond_from_backend(msg).split(" ")
-    tampungRespond = ""
-    for kataRespond in respond:
-        if kataRespond == "~":
-            tampungRespond += "\n\t   "
-            continue
-        elif kataRespond == "*":
-            tampungRespond += "\t"
-            continue
-        tampungRespond += kataRespond + " "
-    return jsonify({"message": tampungRespond})
+    @app.route("/predict", methods=['POST'])
+    def send_message():
+        pesan = request.get_json()["message"]
+        print(pesan)
+        
+        pesanFix = ""
+        pesanArray = pesan.split(" ")
+        for i in pesanArray:
+            pesanFix += correction(i) + " "
+            
+        print("Hasil Koreksi : " + pesanFix)
+        response = bot.get_response(pesanFix)
+        
+        return jsonify({"message": response})
+
+    return app
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5001)
+    create_app().run(debug=True, port=5001)
